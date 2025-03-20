d641f5cb5cf6eebc945d3cdc1989c1399926ba58
user <anuragsinghofficial05@gmail.com>
2025-03-20 15:42:05 +0530

    Update CompanyExpense.jsx

--- src/pages/CompanyExpense.jsx
+++ src/pages/CompanyExpense.jsx
@@ -89,7 +89,7 @@ const CompanyExpense = ({selectedCompany}) => {
         </div>
       </div>
       <div className="mt-4 ">
-        {/* <CompanyChart selectedCompany={selectedCompany}/> */}
+        <CompanyChart selectedCompany={selectedCompany}/>
       </div>
       {/* Three Equal-Height Boxes */}
       <div className="flex w-full mt-16 gap-4">
@@ -125,7 +125,7 @@ const CompanyExpense = ({selectedCompany}) => {
             Subscription cost
           </div>
           <div className="mt-6 flex-1 h-screen">
-            {/* <AccountTable date={selectedMonthYear} selectedCompany={selectedCompany} className="min-h-screen"/> */}
+            <AccountTable date={selectedMonthYear} selectedCompany={selectedCompany} className="min-h-screen"/>
           </div>
         </div>
       </div>
